c3c7facd363cc4a3615419b85c0796c599eba444
ateodocio <teodocio.am@gmail.com>
2022-08-09 20:58:23 -0500

    Refactor script and update html and css

--- rock_paper_scissors/main.js
+++ rock_paper_scissors/main.js
@@ -1,52 +1,69 @@
+// initialize global variables
+let player = 0
+let computer = 0
+
+const roundResult = document.querySelector('.roundResult')
+
+const btns = document.querySelectorAll('button')
+
+
+// listens for button click representing the player's selection
+// calls the playGame function when a click has triggered
+btns.forEach(btn => btn.addEventListener('click', () => playGame(btn.className)))
+
+
+function playGame(playerChoice) {
+    // takes in the player's selection based on button clicked and checks for winner
+    if (player < 5 && computer < 5) {
+        const combos = [["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]]
+        let computerChoice = getComputerChoice()
+
+        if (playerChoice === computerChoice) {
+            roundResult.textContent = "Bruh. It's a tie!"
+
+        } else {
+            for (const i of combos) {
+                if (playerChoice === i[0] && computerChoice ===  i[1]) {
+                    player++
+                    roundResult.textContent = `You win! ${capitalizeWord(i[0])} beats ${capitalizeWord(i[1])}. Yehey! `
+                    updateScore("p")
+
+                } else if (computerChoice === i[0] && playerChoice ===  i[1]) {
+                    computer++
+                    roundResult.textContent = `You lose! ${capitalizeWord(i[0])} beats ${capitalizeWord(i[1])}. Nyam nyam.`
+                    
+                    updateScore("c")
+                }
+            }
+        }
+    } else {
+        roundResult.textContent = 'Play again next time. Thanks! -Ela'
+    }
+}
+
+
+function updateScore(roundWinner) {
+    // expects "p" or "c" that represents the current round's winner and updates the scores in the html page
+    const playerScore = document.querySelector('.player')
+    const compScore = document.querySelector('.comp')
+
+    if (roundWinner === "p") {
+        playerScore.textContent = player
+    } else if (roundWinner === "c") {
+        compScore.textContent = computer
+    }
+}
+
 
 function getComputerChoice() {
+    // returns a randomly-generated choice between rock, paper, or scissors
     const choices = ["rock", "paper", "scissors"]
     const randomNumber = Math.floor((Math.random() * 3))
     return choices[randomNumber]
 }
 
 
-function getPlayerChoice() {
-    const playerChoice = prompt("Rock, paper, scissors! Choose one: ").toLowerCase();
-    return playerChoice
+function capitalizeWord(string) {
+    // expects a string and returns a string with the first letter capitalized
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-function playRound(playerSelection, computerSelection) {
-    const combos = [["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]]
-
-    if (playerSelection === computerSelection) {
-        console.log("It's a tie!")
-    }  else {
-        for (const i of combos) {
-            if (playerSelection === i[0] && computerSelection ===  i[1]) {
-                console.log(`You win! ${i[0]} beats ${i[1]}`)
-                return "p"
-            } else if (computerSelection === i[0] && playerSelection ===  i[1]) {
-                console.log(`You lose! ${i[0]} beats ${i[1]}`)
-                return "c"
-            }
-        }
-    }
-}
-
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-
-    for (let round = 0; round < 5; round++) {
-        console.log(`Current score: Player = ${playerScore} | Computer = ${computerScore}`)
-        console.log(`Round ${round+1}!`)
-
-        let currentRound = playRound(getPlayerChoice(), getComputerChoice())
-
-        if (currentRound === "p") {
-            playerScore++;
-        } else if (currentRound === "c") {
-            computerScore++;
-        }
-    }
-    console.log(`Play again next time! Player = ${playerScore} | Computer = ${computerScore}`)
-}
-
-
-game()
